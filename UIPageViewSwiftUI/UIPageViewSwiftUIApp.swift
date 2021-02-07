c00e4bcdc3aac9c63b119b7ac260bfa2595e1e49
phdafoe <phdafoe@gmail.com>
2021-02-07 09:34:05 +0100

    Initial Commit

---
+++ UIPageViewSwiftUI/UIPageViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPageViewSwiftUIApp.swift
+//  UIPageViewSwiftUI
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 07/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct UIPageViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
